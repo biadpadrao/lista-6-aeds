2c798a67c322e4bde48f351590d2d686b950f49f
biadpadrao <103266259+biadpadrao@users.noreply.github.com>
2022-05-19 15:03:24 -0300

    Update and rename questão 3 to questão 3.C

--- questão 3.C
+++ questão 3.C
@@ -1,10 +1,7 @@
-// Faça um procedimento que preencha 2 vetores X e Y com 10 elementos cada um (ocupando
-as posições de 0 a 9 em cada vetor). Faça um outro procedimento que receba dois vetores
-preenchidos e gera um novo vetor com os elementos desses 2 vetores intercalados de tal
-forma que nas posições ímpares do novo vetor estejam os elementos do primeiro vetor e nas
-posições pares os elementos do segundo vetor recebido por parâmetro. Faça um
-procedimento que receba e exiba o conteúdo de um vetor. Faça um programa que faça as
-devidas declarações e acione os módulos para exemplificar o seu uso.
+// Faça um procedimento que preencha 2 vetores X e Y com 10 elementos cada um (ocupando as posições de 0 a 9 em cada vetor).
+// Faça um outro procedimento que receba dois vetores preenchidos e gera um novo vetor com os elementos desses 2 vetores intercalados
+// de tal forma que nas posições ímpares do novo vetor estejam os elementos do primeiro vetor e nas posições pares os elementos
+// do segundo vetor recebido por parâmetro. Faça um procedimento que receba e exiba o conteúdo de um vetor. 
 
 #include <stdio.h>
 #include <time.h>
